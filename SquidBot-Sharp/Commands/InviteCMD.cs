baa439a7c03a0dc05dc77b568d35059bc53ab3e3
18swenskiq <18swenskiq@gmail.com>
2020-04-09 20:52:16 -0400

    Made invite link not embed on >invite command

--- SquidBot-Sharp/Commands/InviteCMD.cs
+++ SquidBot-Sharp/Commands/InviteCMD.cs
@@ -12,7 +12,7 @@ namespace SquidBot_Sharp.Commands
         [Command("invite"), Description("Invite MechaSquidski to your server!")]
         public async Task Invite(CommandContext ctx)
         {
-            await ctx.RespondAsync("Invite Link: https://discordapp.com/api/oauth2/authorize?client_id=565566309257969668&permissions=103931008&scope=bot");
+            await ctx.RespondAsync("Invite Link: <https://discordapp.com/api/oauth2/authorize?client_id=565566309257969668&permissions=103931008&scope=bot>");
             return;
         }
     }
