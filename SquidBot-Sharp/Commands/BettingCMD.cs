3c4d81d556e7d37cb8a03a9e97301eb885b9ba14
18swenskiq <18swenskiq@gmail.com>
2021-05-27 16:28:59 -0400

    Fixed paths and stuff for remote development on server

---
+++ SquidBot-Sharp/Commands/BettingCMD.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using SquidBot_Sharp.Models;
+using SquidBot_Sharp.Modules;
+
+namespace SquidBot_Sharp.Commands
+{
+    public class BettingCMD : BaseCommandModule
+    {
+        [Command("squidbet"), Description("Bet on something")]
+        [Cooldown(1, 5, CooldownBucketType.User)]
+        public async Task SquidBet(CommandContext ctx)
+        {
+
+        }
+    }
+}
